0baa28dafbbf3459b0d25292fa5d03190fd467d2
Jeff-Aporta <jeffaporta@gmail.com>
2026-06-12 15:03:57 -0500

    feat(core): change widget target style to "chip".

--- js/core/isa-setup.ts
+++ js/core/isa-setup.ts
@@ -4,7 +4,7 @@
     ns: "CONV",
     app: "conversations-front",
     theme: { lsKey: "conversations:theme" },
-    widgets: { targetStyle: "switch" },
+    widgets: { targetStyle: "chip" },
     loginGate: "inline",
   });
 })();
